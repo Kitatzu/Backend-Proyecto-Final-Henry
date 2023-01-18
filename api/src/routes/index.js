aeb40074fd614e14a7d6a390baf53be8bf34cdb8
MarcialChavesta <marcialchavesta@gmail.com>
2023-01-18 03:02:36 -0500

    Rutas de Categorías terminado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const categoriesRouter = require("./categoriesRouter");
+router.use("/categories", categoriesRouter)
 
 module.exports = router;
