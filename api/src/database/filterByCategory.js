1c69cfb5c529bf717eedc18a78b9854c2d70a519
MarcialChavesta <marcialchavesta@gmail.com>
2023-01-19 18:21:44 -0500

    Filtros por marca y categoria

---
+++ api/src/database/filterByCategory.js
@@ -0,0 +1,29 @@
+const { Products, Categories } = require("../db.js");
+
+const productsByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const filterCategories = await Products.findAll({
+      include: {
+        model: Categories,
+        attributes: ["name"],
+        through: { attributes: [] },
+        where: {
+          name: category,
+        },
+      },
+    });
+   
+    filterCategories.length
+      ? res.status(200).json({ Status: "Success", filterCategories })
+      : res
+          .status(400)
+          .json({
+            Status: "Error",
+            Message: "There are no products in the selected category.",
+          });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+module.exports = { productsByCategory };
