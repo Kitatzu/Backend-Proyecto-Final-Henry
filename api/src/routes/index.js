2dabac6fceac14b6cc075cf83d3d7cee3b0dea33
cajadios <santi-ventu@hotmail.com>
2023-01-19 16:25:52 -0300

    search

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,11 @@ const router = Router();
 const ProductsRoutes = require("./ProductsRoutes");
 const categoriesRoutes = require("./categoriesRoutes");
 const providersRoutes = require("./providersRoutes");
+const SearchRouter= require("./SearchRouter")
 
 router.use("/categories", categoriesRoutes);
 router.use("/products", ProductsRoutes);
 router.use("/providers", providersRoutes);
+router.use("/search",SearchRouter);
 
 module.exports = router;
