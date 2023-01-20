c291eab451de39a60015e11819717ba08630051b
Exequiel Rodriguez <109878818+Kitatzu@users.noreply.github.com>
2023-01-20 00:37:23 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const ProductsRoutes = require("./ProductsRoutes");
+const productsRoutes = require("./productsRoutes");
 const categoriesRoutes = require("./categoriesRoutes");
 const providersRoutes = require("./providersRoutes");
 const searchRoutes = require("./SearchRoutes");
@@ -8,7 +8,7 @@ const filterByBrandRoutes = require("./filterByBrandRoutes");
 const filterByCategoryRoutes = require("./filterByCategoriesRoutes");
 
 router.use("/categories", categoriesRoutes);
-router.use("/products", ProductsRoutes);
+router.use("/products", productsRoutes);
 router.use("/providers", providersRoutes);
 router.use("/search", searchRoutes);
 router.use("/brand", filterByBrandRoutes);
