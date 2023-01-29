105cc2b879f1496ffbf3ec2417867a73578190a6
Exequiel Rodriguez <109878818+Kitatzu@users.noreply.github.com>
2023-01-29 02:06:36 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@
 const { server } = require("./src/app.js");
 require("dotenv").config;
 const { PORT } = process.env;
-const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const {
   createRoles,
