993f7475a9665e2dd401d1cf1988b53994157ff8
MarcialChavesta <marcialchavesta@gmail.com>
2023-02-07 14:42:31 -0500

    chat 2.0 finalizado con BBDD

--- api/src/controllers/messageController.js
+++ api/src/controllers/messageController.js
@@ -1,25 +1,17 @@
 const { Messages, Users } = require("../db.js");
 
 const createMessage = async (userName, messageContent) => {
-  console.log("messagemessageContent");
-  //TODO: LA FUNCION QUE SE DEBE USAR PARA MESSAGE ES SETUSER, AUN ASI SETEAMOS EL USERID
-  //FIXME: REVISAR NOMBRES DE LAS PROPIEDADES PARA MAPEAR EN EL FRONT
-  //FIXME: LAS PROPIEDADES QUE LLEGAN EN EL BACK ES EL OBJETO {user,content,avatar} y solo se retorna el user y el content OJO , en el front se reciven datos con propiedades diferentes.
   try {
     const user = await Users.findOne({
       where: {
         userName: userName,
       },
     });
-    console.log(user);
+   
     const message = await Messages.create({
       content: messageContent,
       userId: user.id,
-    });
-
-    //FIXME: NO EXISTE FUNCION
-    // await user.addMessags(message);
-
+    });  
     return {
       userName: userName,
       message: message.content,
@@ -34,9 +26,13 @@ const getMessages = async () => {
     const messages = await Messages.findAll({
       include: {
         model: Users,
-        attributes: ["userName"],
+        attributes: ["userName", "avatar"],
+        /* through: {
+          attributes: [],
+        }, */
       },
     });
+    /* console.log('a ver que sake',messages); */
     return messages;
   } catch (error) {
     throw new Error(error.message);
